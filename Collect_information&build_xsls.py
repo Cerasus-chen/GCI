9390694684b125b629a4a04cbca250f7da65086d
EverlastingVidfyoi <TKcyt@hotmail.com>
2020-09-03 16:10:35 +0800

    完善python处理数据转变为Excel

--- Collect_information&build_xsls.py
+++ Collect_information&build_xsls.py
@@ -1,14 +1,17 @@
 import os
 import json
+import xlwt
 import re
+
 def Get_files(Path):
     Files_list = os.listdir(Path)
     Files_list.remove('script')
     return  Files_list
 
-def handle_information(information):
+def handle_information(id,information):
     information = information.readlines()
-    Item = {}
+    item = {}
+    item["资产编号"] = id
     title = ['中文名字：\n','英文名字：\n','CPU信息：\n','主板信息：\n','内存信息：\n','硬盘信息：\n','网卡MAC：\n','ip地址：\n','显卡信息：\n','显示器信息：\n']
     for Name in title:
         if (Name == "内存信息：\n"):
@@ -20,7 +23,8 @@ def handle_information(information):
             inf = information[Mem_Inf_Index_Begin:Mem_Inf_Index_End]
             Name = Name.replace("\n", "")
             inf = [x.strip() for x in inf if x.strip() != '']
-            Item[Name] = inf
+            item[Name] = inf
+
         elif (Name == "硬盘信息：\n"):
             Disk_Count_Index = information.index("硬盘数量：\n")
             Disk_Count = information[Disk_Count_Index + 1]
@@ -30,7 +34,8 @@ def handle_information(information):
             inf = information[Disk_Inf_Index_Begin:Disk_Inf_Index_End]
             Name = Name.replace("\n", "")
             inf = [x.strip() for x in inf if x.strip() != '']
-            Item[Name] = inf
+            item[Name] = inf
+
         elif (Name == "显示器信息：\n"):
             Monitor_Count_Index = information.index("显示器数量：\n")
             try:
@@ -45,23 +50,55 @@ def handle_information(information):
                 inf = information[Monitor_Inf_Index_Begin:Monitor_Inf_Index_End]
                 Name = Name.replace("\n", "")
                 inf = [x.strip() for x in inf if x.strip() != '']
-                Item[Name] = inf
+                x=[]
+                for i in inf:
+                    i = re.sub(u'\u0000', "", i)
+                    x.append(i)
+                item[Name] = x
+
         else:
             i = information.index(Name)
             Name = Name.replace("\n","")
-            Item[Name] = information[i+1].replace("\n","")
-    return Item
+            item[Name] = information[i+1].replace("\n","")
 
+    return item
 
+def save_json_file(datas):
+    file_name= "2.json"
+    file_path= "./"+file_name
+    json.dump(datas,open(file_path,"a+",encoding="utf-16"),ensure_ascii=False,indent=4)
+
+def build_excel_file(path):
+    file_path = path
+    json_file = open(file_path,"r",encoding="utf-16")
+    computer_inf = json.load(json_file)
+    workbook = xlwt.Workbook()
+    sheet1 = workbook.add_sheet('sheet1')
+    ll =list(computer_inf[0].keys())
+    #print(ll)
+    for i in range(0,len(ll)):
+        sheet1.write(0,i,ll[i])
+    for j in range(0,len(computer_inf)):
+        m =0
+        ls = list(computer_inf[j].values())
+        for k in ls:
+            sheet1.write(j+1,m,k)
+            m +=1
+    workbook.save('computer_inf.xls')
 
 if __name__ == '__main__':
+    json_file_path = "./2.json"
     Path = 'E:/information/'
     Files_Name = Get_files(Path)
-    Information ={}
+    computer_inf = []
     for i in Files_Name:
         File_Path = Path+i
         inf = open(File_Path,encoding='utf-16')
-        datas = handle_information(inf)
+        datas = handle_information(i,inf)
         inf.close()
+        computer_inf.append(datas)
         print(datas)
+    save_json_file(computer_inf)
+    build_excel_file(json_file_path)
+
 
